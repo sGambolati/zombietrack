8acd8d3da647281b414c72af77086d3e83f7c324
Sebastian Ariel Gambolati <sebastian.gambolati@southworks.com>
2017-05-11 18:33:23 -0300

    Add Finish Spatial UnderStanding.

--- Assets/Scripts/SpatialUnderstandingState.cs
+++ Assets/Scripts/SpatialUnderstandingState.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using UnityEngine;
 using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
 using HoloToolkit.Unity.SpatialMapping;
 
-public class SpatialUnderstandingState : Singleton<SpatialUnderstandingState>
+public class SpatialUnderstandingState : Singleton<SpatialUnderstandingState>, IInputClickHandler, ISourceStateHandler
 {
     public float MinAreaForStats = 5.0f;
+    public float MinAreaForComplete = 50.0f;
+    public float MinHorizAreaForComplete = 25.0f;
+    public float MinWallAreaForComplete = 10.0f;
+
+    private uint trackedHandsCount = 0;
 
     public TextMesh DebugDisplay;
     public TextMesh DebugSubDisplay;
 
     private bool _triggered;
     public bool HideText = false;
+
+    private bool ready = false;
 
     private string _spaceQueryDescription;
 
@@ -24,6 +32,38 @@ public class SpatialUnderstandingState : Singleton<SpatialUnderstandingState>
         set
         {
             _spaceQueryDescription = value;
+        }
+    }
+
+
+
+    public bool DoesScanMeetMinBarForCompletion
+    {
+        get
+        {
+            // Only allow this when we are actually scanning
+            if ((SpatialUnderstanding.Instance.ScanState != SpatialUnderstanding.ScanStates.Scanning) ||
+                (!SpatialUnderstanding.Instance.AllowSpatialUnderstanding))
+            {
+                return false;
+            }
+
+            // Query the current playspace stats
+            IntPtr statsPtr = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStatsPtr();
+            if (SpatialUnderstandingDll.Imports.QueryPlayspaceStats(statsPtr) == 0)
+            {
+                return false;
+            }
+            SpatialUnderstandingDll.Imports.PlayspaceStats stats = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStats();
+
+            // Check our preset requirements
+            if ((stats.TotalSurfaceArea > MinAreaForComplete) ||
+                (stats.HorizSurfaceArea > MinHorizAreaForComplete) ||
+                (stats.WallSurfaceArea > MinWallAreaForComplete))
+            {
+                return true;
+            }
+            return false;
         }
     }
 
@@ -53,6 +93,11 @@ public class SpatialUnderstandingState : Singleton<SpatialUnderstandingState>
                             return "playspace stats query failed";
                         }
 
+                        // The stats tell us if we could potentially finish
+                        if (DoesScanMeetMinBarForCompletion)
+                        {
+                            return "When ready, air tap to finalize your playspace";
+                        }
                         return "Walk around and scan in your playspace";
                     case SpatialUnderstanding.ScanStates.Finishing:
                         return "Finalizing scan (please wait)";
@@ -70,7 +115,23 @@ public class SpatialUnderstandingState : Singleton<SpatialUnderstandingState>
     {
         get
         {
-            return Color.white;
+            ready = DoesScanMeetMinBarForCompletion;
+            if (SpatialUnderstanding.Instance.ScanState == SpatialUnderstanding.ScanStates.Scanning)
+            {
+                if (trackedHandsCount > 0)
+                {
+                    return ready ? Color.green : Color.red;
+                }
+                return ready ? Color.yellow : Color.white;
+            }
+
+            // If we're looking at the menu, fade it out
+            float alpha = 1.0f;
+
+            // Special case processing & 
+            return (!string.IsNullOrEmpty(SpaceQueryDescription)) ?
+                (PrimaryText.Contains("processing") ? new Color(1.0f, 0.0f, 0.0f, 1.0f) : new Color(1.0f, 0.7f, 0.1f, alpha)) :
+                new Color(1.0f, 1.0f, 1.0f, alpha);
         }
     }
 
@@ -123,10 +184,40 @@ public class SpatialUnderstandingState : Singleton<SpatialUnderstandingState>
         DebugSubDisplay.text = DetailsText;
     }
 
+    private void Start()
+    {
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+    }
+
     // Update is called once per frame
     private void Update()
     {
         // Updates
         Update_DebugDisplay();
+
+        if (!_triggered && SpatialUnderstanding.Instance.ScanState == SpatialUnderstanding.ScanStates.Done)
+        {
+            _triggered = true;
+        }
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (ready &&
+            (SpatialUnderstanding.Instance.ScanState == SpatialUnderstanding.ScanStates.Scanning) &&
+            !SpatialUnderstanding.Instance.ScanStatsReportStillWorking)
+        {
+            SpatialUnderstanding.Instance.RequestFinishScan();
+        }
+    }
+
+    void ISourceStateHandler.OnSourceDetected(SourceStateEventData eventData)
+    {
+        trackedHandsCount++;
+    }
+
+    void ISourceStateHandler.OnSourceLost(SourceStateEventData eventData)
+    {
+        trackedHandsCount--;
     }
 }
